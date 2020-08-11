81303fd393623b0a986b1842731da4393924238f
Patricio Cerda Mardini <pcerdam@live.com>
2020-08-11 19:29:20 -0400

    working, but only on cholesterol dataset

--- frameworks/MindsDB/exec.py
+++ frameworks/MindsDB/exec.py
@@ -1,11 +1,11 @@
 import logging
 import pandas as pd
+import numpy as np
 
 from mindsdb import Predictor
 from scipy.io.arff import loadarff
 
-from amlb.results import save_predictions_to_file
-from frameworks.shared.callee import call_run, result  # , output_subdir, utils
+from frameworks.shared.callee import call_run, result
 
 
 log = logging.getLogger(__name__)
@@ -18,31 +18,23 @@ def run(dataset, config):
 
     X_train = pd.DataFrame(loadarff(dataset.train.path)[0])
     X_test = pd.DataFrame(loadarff(dataset.test.path)[0])
+    queries = X_test
 
     predictor = Predictor(name="MindsDB")
     predictor.learn(from_data=X_train, to_predict=target)
 
-    save_predictions(predictor, X_test, target, config, dataset=dataset)
-    # save_artifacts() here
-
-    return result(output_file=config.output_predictions_file,
-                  #predictions=None, probabilities=None, truth=None)
-                  )
-
-def save_predictions(predictor, queries, target, config, dataset, predictions_file=None, preview=True):
     predictions = predictor.predict(when_data=queries)
     predictions = [x.explanation for x  in predictions]
 
-    pred_values = [x[target]['predicted_value'] for x in predictions]
-    probabilities = [x[target]['confidence'] for x in predictions],
-    truth = dataset[target]
+    preds = np.array([x[target]['predicted_value'] for x in predictions])
+    probs = np.array([x[target]['confidence'] for x in predictions])
+    truth = queries[target].values
 
-    save_predictions_to_file(dataset=dataset,
-                             output_file=config.output_predictions_file if predictions_file is None else predictions_file,
-                             probabilities=probabilities,
-                             predictions=pred_values,
-                             truth=truth,
-                             preview=preview)
+    return result(output_file=config.output_predictions_file,
+                  predictions=preds,
+                  # probabilities=probs,
+                  truth=truth
+                  )
 
 
 if __name__ == '__main__':
