e186ce616bc03af82dc0cf612c5b9ca8d4d6e8c5
Patricio Cerda Mardini <pcerdam@live.com>
2020-08-11 18:35:39 -0400

    cleanup, comment troublesome pip instruction. currently does not work, needs manual venv setup

--- frameworks/MindsDB/setup.sh
+++ frameworks/MindsDB/setup.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-HERE=$(dirname "$0")  #/home/pcerdam/Documents/MindsDB/automlbenchmark/frameworks/MindsDB
-AMLB_DIR="$1"  # /home/pcerdam/Documents/MindsDB/automlbenchmark
-VERSION=${2:-"latest"} # 2.1.2
+HERE=$(dirname "$0")
+AMLB_DIR="$1"
+VERSION=${2:-"latest"}
 if [[ "$VERSION" == "latest" ]]; then
     VERSION="stable"
 fi
-REPO=${3:-"https://github.com/mindsdb/mindsdb"}  # https://github.com/mindsdb/mindsdb
-PKG=${4:-"mindsdb"}  # mindsdb
+REPO=${3:-"https://github.com/mindsdb/mindsdb"}
+PKG=${4:-"mindsdb"}
 RAWREPO=$(echo ${REPO} | sed "s/github\.com/raw\.githubusercontent\.com/")
 
 # creating local venv
 . $HERE/../shared/setup.sh $HERE
 
 PIP install --no-cache-dir -U -r "${RAWREPO}/${VERSION}/requirements.txt"
-PIP install --no-cache-dir ${PKG}==${VERSION}
+# PIP install --no-cache-dir ${PKG}==${VERSION}
