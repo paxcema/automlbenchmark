a4cb728d6759586cace89f09c85c19bcb937cf59
Patricio Cerda Mardini <pcerdam@live.com>
2020-08-12 17:39:46 -0400

    Now all 3 predict sensible values

--- frameworks/MindsDB/__init__.py
+++ frameworks/MindsDB/__init__.py
@@ -1,5 +1,6 @@
 from amlb.utils import call_script_in_same_dir
 from frameworks.shared.caller import run_in_venv
+from amlb.datautils import impute
 
 
 def setup(*args, **kwargs):
@@ -7,10 +8,17 @@ def setup(*args, **kwargs):
 
 
 def run(dataset, config):
+    X_train_enc, X_test_enc = impute(dataset.train.X_enc, dataset.test.X_enc)
     data = dict(
             target=dict(name=dataset.target.name),
-            train=dict(path=dataset.train.path),
-            test=dict(path=dataset.test.path)
+            train=dict(path=dataset.train.path,
+                       X_enc=X_train_enc,
+                       y_enc=dataset.train.y_enc
+                       ),
+            test=dict(path=dataset.test.path,
+                      X_enc=X_test_enc,
+                      y_enc=dataset.test.y_enc
+                      )
     )
 
     return run_in_venv(__file__, "exec.py",
--- frameworks/MindsDB/exec.py
+++ frameworks/MindsDB/exec.py
@@ -16,9 +16,30 @@ def run(dataset, config):
 
     target = dataset.target.name
 
-    X_train = pd.DataFrame(loadarff(dataset.train.path)[0])
-    X_test = pd.DataFrame(loadarff(dataset.test.path)[0])
-    queries = X_test
+    temp_train = pd.DataFrame(loadarff(dataset.train.path)[0])
+    temp_test = pd.DataFrame(loadarff(dataset.test.path)[0])
+
+    X_train = pd.DataFrame(dataset.train.X_enc)
+    X_test = pd.DataFrame(dataset.test.X_enc)
+
+    y_train = pd.DataFrame(dataset.train.y_enc)
+    y_test = pd.DataFrame(dataset.test.y_enc)
+
+    X_train[target] = y_train
+    X_test[target] = y_test
+
+    X_train.columns = temp_train.columns
+    X_test.columns = temp_test.columns
+
+    log.info("TRAIN")
+    log.info(temp_train.head())
+    log.info(X_train.head())
+    log.info(y_train.head())
+
+    log.info("TEST")
+    log.info(temp_test.head())
+    log.info(X_test.head())
+    log.info(y_test.head())
 
     predictor = Predictor(name="MindsDB")
     predictor.learn(from_data=X_train,
@@ -26,12 +47,12 @@ def run(dataset, config):
                     stop_training_in_x_seconds=config.max_runtime_seconds,
                     )
 
-    predictions = predictor.predict(when_data=queries)
+    predictions = predictor.predict(when_data=X_test)
     predictions = [x.explanation for x  in predictions]
 
     preds = np.array([x[target]['predicted_value'] for x in predictions])
     probs = np.array([x[target]['confidence'] for x in predictions])
-    truth = queries[target].values
+    truth = X_test[target].values
 
     return result(output_file=config.output_predictions_file,
                   predictions=preds,
