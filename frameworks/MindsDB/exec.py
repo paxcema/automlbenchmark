91bc7ee9dabf568c26138d2408ea0b7c97d0782a
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2021-01-13 23:36:23 +0000

    handle edge case

--- frameworks/MindsDB/exec.py
+++ frameworks/MindsDB/exec.py
@@ -1,3 +1,4 @@
+import traceback
 import sys
 sys.path.insert(0, '/home/ubuntu/experiments/mindsdb_native')
 sys.path.insert(0, '/home/ubuntu/experiments/lightwood')
@@ -46,22 +47,27 @@ def run(dataset, config):
                           stop_training_in_x_seconds=config.max_runtime_seconds)
 
     results = predictor.predict(when_data=X_test)._data
+    print(results)
     truth = X_test[target].values
 
     if is_classification:
         model_data = get_model_data("MindsDB")
-        beliefs = results[f'{target}_class_distribution']
-        idx2cls = results[f'{target}_class_map']
-        cls2idx = {v:k for k, v in idx2cls.items()}
-        print(beliefs)
         classes = sorted(model_data['data_analysis_v2'][target]['histogram']['x'])
-        print(classes)
-        beliefs = np.array([[b[cls2idx[c]] for c in classes] for b in beliefs])
         preds = np.array([classes[int(float(i))] for i in results[target]])
+
+        if f'{target}_class_map' in results:
+            beliefs = results[f'{target}_class_distribution']
+            idx2cls = results[f'{target}_class_map']
+            cls2idx = {v:k for k, v in idx2cls.items()}
+            beliefs = np.array([[b[cls2idx[c]] for c in classes] for b in beliefs])
+        else:
+            pidxs = preds.astype(np.float).astype(np.int)
+            beliefs = np.eye(len(classes))[pidxs]
+
+        print(beliefs)
         preds = pd.DataFrame(np.hstack([beliefs, preds.reshape(-1, 1)]))
         preds.columns = classes + ['predictions']
         preds['truth'] = truth
-        print(preds)
         preds.to_csv(config.output_predictions_file, index=False)
 
     else:
